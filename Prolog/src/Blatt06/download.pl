e6a0c28cb4d1c41df99f0edeacf1aba1882bfa85
Ulrich Rott <ulrich.rott@st.ovgu.de>
2013-05-20 21:57:12 +0200

    Add Blatt06

---
+++ Prolog/src/Blatt06/download.pl
@@ -0,0 +1,18 @@
+%	song,		interpret,			rank,	days,	price
+song(on_the_Floor, 	jennifer_lopez, 		1, 	72, 	0.98)
+song(sweat, 		snoop_dogg_vs._david_guetta, 	2, 	56,	0.98)
+song(a_night_like_this,	caro_emerald,			3,	65,	0.98)
+song(hollywood_hills,	sunrice_avenue,			4,	104,	0.98)
+song(s_and_m,		rhianna,			5,	103,	0.98)
+song(you_and_me,	milow,				6,	48,	0.98)
+song(hello,		martin_Solveig _and_Dragonette,	7,	183,	0.91)
+song(born_this_way,	lady_gaga,			8,	83,	0.98)
+song(more,		usher,				9,	93,	0.99)
+song(still,		jupiter_jones,			10,	69,	0.68)
+
+% a) song(X,_,3,_,_).
+% b) song(X,usher,_,_,_).
+% c) song(you_and_me, X,_,_,_).
+% d) song(X,_,_,183,_).
+% e) song(X,_,_,_,0.98).
+% f) song(X,_,_,Y,_), Y > 100.
