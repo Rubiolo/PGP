56a6bb8a6d52f23efcefee52766ce3ae920d2fe0
Ulrich Rott <ulrich.rott@st.ovgu.de>
2013-05-20 22:41:18 +0200

    Add tree.pl

---
+++ Prolog/src/Blatt06/tree.pl
@@ -0,0 +1,27 @@
+mann(anton).
+mann(hugo).	
+mann(fritz).
+mann(heinz).
+mann(wilhelm).
+mann(alfons).
+
+frau(wilhelmine).
+frau(elfriede).
+frau(maria).
+frau(brunhilde).
+frau(theresia).
+
+mutter(wilhelmine, anton).
+mutter(wilhelmine, theresia).
+
+mutter(brunhilde,wilhelm).
+
+mutter(elfriede, maria).
+mutter(elfriede, hugo).
+
+mutter(theresia, alfons).
+
+verheiratet(wilhelmine,fritz).
+verheiratet(brunhilde,heinz).
+verheiratet(elfriede,anton).
+verheiratet(theresia,wilhelm).
