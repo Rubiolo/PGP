2206396e8dcafd2217431cd0da9a0ca7c2b15a65
Ulrich Rott <ulrich.rott@st.ovgu.de>
2013-05-27 19:07:20 +0200

    correct simplequest

---
+++ Prolog/src/Blatt05/simplerequest.pl
@@ -0,0 +1,27 @@
+Stellen Sie eine kleine Wissensbasis von Tieren und Jaeger-Beute-Paaren nach dem Muster tier(katze) und jagt(katze, maus) zusammen. Stellen Sie dann die folgenden Anfragen:
+
+tier(katze).
+tier(maus).
+tier(hund).
+
+jagt(katze, maus).
+jagt(hund, katze).
+jagt(hund, maus).
+
+% Welche Tiere jagen Mäuse?
+%jagt(X,maus).
+
+% Welche Tiere jagen Katzen?
+%jagt(X,katze).
+
+% Welches Tier ist sowohl Jäger als auch Beutetier?
+%jagt(X,_),jagt(_,X).
+
+% Welches Tier ist Beutetier?
+%jagt(_,X).
+
+% Welches Tier ist nur Jäger?
+%jagt(X,_),not(jagt(_,X)).
+
+% Wer jagt wen?
+%jagt(X,Y).
