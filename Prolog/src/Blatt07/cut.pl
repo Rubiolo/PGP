86389f5610c1ac0385b26edeef2f372973773460
Ulrich Rott <ulrich.rott@st.ovgu.de>
2013-05-28 18:21:51 +0200

    add cut.pl uncompleted

---
+++ Prolog/src/Blatt07/cut.pl
@@ -0,0 +1,31 @@
+% a) Gegeben sind folgende Definitionen:
+
+obst(apfel).
+
+obst(pfirsich) :- !.
+
+obst(birne).
+
+% explain the result:
+% 1.) obst(X).
+% result: X =  apfel, X = pfirsich.
+% Der Operator "!" fuehrt dazu, dass bei einer Suche nach dem "X" die Weitersuche nach Alternativloesungen
+% bei dem Operator "!" abgebrochen wird. Dadurch wird hier bei der Ausgabe auch kein "X = birne" angezeigt.
+
+% 2.) obst(X),obst(Y).
+% result:
+% X = Y, Y = apfel ;
+% X = apfel, Y = pfirsich ;
+% X = pfirsich, Y = apfel ;
+% X = Y, Y = pfirsich.
+% Hier aehnlich wie beim ersten Beispiel. Wir suchen "X" und "Y", wobei beides laut Definition "obst" sein muessen.
+% Die Suche nach Alternativloesungen endet hier wieder kurz vor obst(birne). aufgrund des Operators "!".
+
+% 3.) obst(X),!,obst(Y).
+% result:
+X = Y, Y = apfel ;
+X = apfel, Y = pfirsich.
+% Hier werden weitere Alternativen nach dem obst(apfel). nicht mehr beachtet, da nur Paare gefunden werden,
+% welche als "X = apfel" haben. Spriche: nur das erste zutreffendes Element wird behandelt.
+
+% b) Definieren Sie das Praedikat class(N,C)
