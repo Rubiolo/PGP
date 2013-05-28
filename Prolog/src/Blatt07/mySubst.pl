6456905c8397ec17d8ed08716e0df6777a848f5a
Ulrich Rott <ulrich.rott@st.ovgu.de>
2013-05-28 19:47:41 +0200

    first push

---
+++ Prolog/src/Blatt07/mySubst.pl
@@ -0,0 +1,14 @@
+% a)
+mySubstOne(_,_,[],[]).
+mySubstOne(E, S, [E|L], [S|N]) :- same(L,N), !.
+mySubstOne(E, S, [H|L], [H|NList]) :- H \= E, mySubstOne(E,S,L,NList).
+
+% helpfunction
+same([],[]).
+same([H|L], [H|N]) :- areEqual(L,N).
+
+% b)
+mySubstAll(_,_,[],[]).
+mySubstAll(E, S, [E|L], [S|NList]) :- mySubstAll(E, S, L, NList), !.
+mySubstAll(E, S, [H|L], [H|NList]) :- H \= E, mySubstAll(E, S, L, NList).
+
