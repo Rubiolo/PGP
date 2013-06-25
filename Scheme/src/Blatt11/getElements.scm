1260f031fad887536f1c28928c2dbff165e4641d
THEpROTTotype <ulrich.rott@st.ovgu.de>
2013-06-25 12:45:18 +0200

    complete

--- Scheme/src/Blatt11/getElements.scm
+++ Scheme/src/Blatt11/getElements.scm
@@ -3,33 +3,27 @@
 	(reco2 String "" 0)
 )
 
-; adding a symbol to  a String O -> " |O|"
+; adding a symbol to a String O -> " |O|"
 (define (add2 String toADD)
-	(string-append String (string-append " |" (string-append toADD "|")))
+	(string-append String (string-append " |" (string-append (make-string 1 toADD) "|")))
 )
 
 
 ; recursive searching for a symbol and adding to result-String
 (define (reco2 String result k)
-	(if (eqv? k (- (string-length String) 1))
+	(if (eq? k (string-length String))
 		result
-		(if (symbol? (string-ref String k))
-			(reco2 String (add2 result (string-ref String k)) (+ k 1))
+		(if (nummber? (string-ref String k))
 			(reco2 String result (+ k 1))
+			(if (char-whitespace? (string-ref String k))
+				(reco2 String result (+ k 1))
+				(reco2 String (add2 result (string-ref String k)) (+ k 1))
+			)
 		)
 	)
 )
 
-string-length "String"
-string-ref "String" k
-
-; comments
-(add2 "d" "1")
-
-(add2 "|O|" "I")
-
-reco2 "C6 H12 O6" "" 0)
-
-(symbol? (string-ref "1H" 0))
-
-(getElements "C6 H12 O6")
+; function control: is a char a number? number are{#\1, #\2, #\3, #\4, #\5, #\6, #\7, #\8, #\9}
+(define (nummber? oneString)
+	(or (or (or (or (or (or (or (or (or (char=? oneString #\1) (char=? oneString #\2)) (char=? oneString #\3)) (char=? oneString #\4)) (char=? oneString #\5)) (char=? oneString #\6)) (char=? oneString #\6)) (char=? oneString #\7)) (char=? oneString #\8)) (char=? oneString #\9))
+)
