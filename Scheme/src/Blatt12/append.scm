7287001e4edac7e23d36fa990f18358ef5b4d10a
THEpROTTotype <ulrich.rott@st.ovgu.de>
2013-07-01 21:18:50 +0200

    complete

--- Scheme/src/Blatt12/append.scm
+++ Scheme/src/Blatt12/append.scm
@@ -1,6 +1,7 @@
 ; main fuction
-(define (appendd list1 list2)
-	(set! list2 list1), list1
+(define (append! a b)
+	(if (null? (cdr a))
+		(set-cdr! a b)
+		(append! (cdr a) b)
+	)
 )
-
- (appendd '(1 2 3) '(5 6 7))  
