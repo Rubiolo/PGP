637ca1ac6b1dba96af3be935677bc548661e1357
THEpROTTotype <ulrich.rott@st.ovgu.de>
2013-07-01 18:13:32 +0200

    add a)

--- Scheme/src/Blatt12/mutation.scm
+++ Scheme/src/Blatt12/mutation.scm
@@ -2,8 +2,29 @@
 
 (define b '(3))
 
-(set-car! (cdr 'b) 'a)
+; a)
+; 1 b: '((1 2))
+(set-car! b a)
+
+; 2 b: '((2))
+(set-car! b (cdr (car b)))
+
+; 3 b: '(3 1 2)
+(set! b (cons '3 a)
+)
+
+; 4 b: '(3 2)
+(set! b (cons (car b) (car(cdr (cdr b)))))
+
 ; b)
 (define x '(a c))
 
 (define y '(b))
+
+; x '(a b c)
+(set-cdr! y (cdr x))
+(set! x (car x))
+(set-cdr! x y)
+
+
+(append (list('3)) a))
