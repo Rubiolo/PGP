01625286973043b9190ddf174dfe113c399c60ad
THEpROTTotype <ulrich.rott@st.ovgu.de>
2013-07-01 21:20:16 +0200

    change variables and add comment

--- Scheme/src/Blatt12/append.scm
+++ Scheme/src/Blatt12/append.scm
@@ -1,7 +1,7 @@
 ; main fuction
-(define (append! a b)
-	(if (null? (cdr a))
-		(set-cdr! a b)
-		(append! (cdr a) b)
+(define (append! list1 list2)
+	(if (null? (cdr list1))
+		(set-cdr! list1 list2)
+		(append! (cdr list1) list2)
 	)
 )
